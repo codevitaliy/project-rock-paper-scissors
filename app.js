e007ec24267ceec9638a234c5e73617576ace737
codevitaliy <codevitaliy@gmail.com>
2022-08-22 19:53:15 +0200

    added computer and player choice also added one play round

---
+++ app.js
@@ -0,0 +1,37 @@
+
+let randomChoice = ["Rock", "Paper", "Scissors"];
+
+const getComputerChoice = () => {
+    let randomChoice = ["rock", "paper", "scissors"];
+    return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+}
+let playerSelection = "rock".toLowerCase();
+let computerSelection = getComputerChoice();
+// console.log(playerSelection);
+
+
+const playSingleRound = (playerSelection, computerSelection) => {
+
+    switch(true) {
+        case (playerSelection === computerSelection):
+        return `You both selected the same ${playerSelection}`;
+        
+        case (playerSelection === "rock" && computerSelection === "scissors" ||
+              playerSelection === "paper" && computerSelection === "rock" ||
+              playerSelection === "scissors" && computerSelection === "paper"):
+        return `Player wins ${playerSelection} beats ${computerSelection}`;
+        
+        case (computerSelection === "rock" && playerSelection === "scissors" ||
+              computerSelection === "paper" && playerSelection === "rock" ||
+              computerSelection === "scissors" && playerSelection === "paper"):
+        return `Computer wins ${computerSelection} beats ${playerSelection}`;
+
+        default:
+            return "an error has ocurred";
+    
+
+    }
+
+}
+
+// console.log(playSingleRound(playerSelection, computerSelection));
