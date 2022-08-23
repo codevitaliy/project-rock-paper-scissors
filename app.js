b8ba85a7b0f585bfc70a08d3d7d45509bdfc1299
codevitaliy <codevitaliy@gmail.com>
2022-08-23 10:50:47 +0200

    js saved file

--- app.js
+++ app.js
@@ -1,42 +1,62 @@
 
-let randomChoice = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+
 
 const getComputerChoice = () => {
     let randomChoice = ["rock", "paper", "scissors"];
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
-let playerSelection = "rock".toLowerCase();
-let computerSelection = getComputerChoice();
-// console.log(playerSelection);
-
 
 const playSingleRound = (playerSelection, computerSelection) => {
 
+    
+    playerSelection = prompt("What is your choice?").toLowerCase();
+    computerSelection = getComputerChoice();
+     
     switch(true) {
         case (playerSelection === computerSelection):
-        return `You both selected the same ${playerSelection}`;
+            return `You both selected the same ${playerSelection}`;
+            
+            case (playerSelection === "rock" && computerSelection === "scissors" ||
+            playerSelection === "paper" && computerSelection === "rock" ||
+            playerSelection === "scissors" && computerSelection === "paper"):
+            playerScore++;
+            return `Player wins ${playerSelection} beats ${computerSelection}`;
+            
+            case (computerSelection === "rock" && playerSelection === "scissors" ||
+            computerSelection === "paper" && playerSelection === "rock" ||
+            computerSelection === "scissors" && playerSelection === "paper"):
+            computerScore++;
+            return `You lose computer choice ${computerSelection} beats ${playerSelection}`;
+            
+            default:
+                return "an error has ocurred";
+                
+            }
+        }
         
-        case (playerSelection === "rock" && computerSelection === "scissors" ||
-              playerSelection === "paper" && computerSelection === "rock" ||
-              playerSelection === "scissors" && computerSelection === "paper"):
-        return `Player wins ${playerSelection} beats ${computerSelection}`;
-        
-        case (computerSelection === "rock" && playerSelection === "scissors" ||
-              computerSelection === "paper" && playerSelection === "rock" ||
-              computerSelection === "scissors" && playerSelection === "paper"):
-        return `Computer wins ${computerSelection} beats ${playerSelection}`;
 
-        default:
-            return "an error has ocurred";
-    
+
+const game = () => {
+    for (let i = 0; i < 5; i++){
+        console.log(playSingleRound(i));
+        console.log(`the player score is ${playerScore}`);
+        console.log(`the computer score is ${computerScore}`);       
+}
+    if (playerScore > computerScore){
+    console.log("You won!");
+    } else if (playerScore < computerScore) {
+    console.log("You lose! computer always win!");
+    } else {
+    console.log("Its a draw");
     }
 }
+    
 
-// console.log(playSingleRound(playerSelection, computerSelection));
 
-for (let i = 0; i < 5; i++) {
-    playSingleRound(playerSelection, computerSelection);
-    console.log(playSingleRound(playerSelection, computerSelection))
-}
 
-console.log(playSingleRound());
+
+game();
+
